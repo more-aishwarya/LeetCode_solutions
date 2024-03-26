9ad93f4bc22e765f1f5627e50ee10e1b436754b9
Aishwarya More <35859174+more-aishwarya@users.noreply.github.com>
2024-03-26 15:44:24 -0400

    Time: 63 ms (59.93%) | Memory: 63.4 MB (6.18%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+           
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
