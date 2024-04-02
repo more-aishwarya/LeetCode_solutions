dc85b6854f1b292e0ce8778420423c44bd044da4
Aishwarya More <35859174+more-aishwarya@users.noreply.github.com>
2024-04-01 20:35:11 -0400

    Time: 3 ms (34.40%) | Memory: 41.5 MB (77.22%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+          HashMap<Integer, Integer> hm= new HashMap<>();
+        for(int i: arr) {
+            if(hm.containsKey(i)) {
+                hm.put(i, hm.get(i)+1);
+            } else {
+                hm.put(i, 1);
+            }
+        }
+        HashSet<Integer> hs= new HashSet<>(hm.values());
+        return hm.size()== hs.size();
+    }
+}
